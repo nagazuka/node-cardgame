9e9af13db1439b298dac128e40a9e7720d28fb83
Shanny Anoep <s.anoep@gmail.com>
2013-02-05 12:44:23 +0000

    Partially implemented askMove

--- node/database.js
+++
@@ -1,9 +0,0 @@
-//var redis = require('redis');
-//var client = redis.createClient();
-
-//dummy operation
-var incr = function(field, callback) {
-    callback(null, 1);
-}
-
-module.exports = {incr: incr}

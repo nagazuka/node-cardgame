5d7ffed5f77a2697118df396e236d5086e28f15f
Shanny Anoep <s.anoep@gmail.com>
2013-02-06 12:42:10 +0000

    Separated game state to allow multiple simultaneous sessions

--- server/game.js
+++ server/game.js
@@ -6,15 +6,17 @@ var _ = require('underscore'),
   cards = require('./cards'),
   rules = require('./rules');
 
+//constants
+var humanPlayerIndex = 0;
+var humanPlayerId = 'A1';
+
 // Class definitions
 var HandInfo = require('./handinfo');
 var Card = cards.Card;
 var Deck = cards.Deck;
 var PlayerMove = cards.PlayerMove;
 
-// Game state variables
-var humanPlayerIndex = 0;
-var humanPlayerId = 'A1';
+/*
 var playingOrder = [0, 1, 2, 3];
 var playerList = [];
 var playerCards = {};
@@ -22,189 +24,200 @@ var trumpSuit;
 var hand = new HandInfo();
 var teamScores = {};
 var deck;
+*/
+
+// Game state variables
+exports.GameState = function() {
+  this.playingOrder = [0, 1, 2, 3];
+  this.playerList = [];
+  this.playerCards = {};
+  this.trumpSuit;
+  this.hand = new HandInfo();
+  this.teamScores = {};
+  this.deck;
+}
 
 //Exposed methods
 
-exports.isReady = function(req, res) {
-  if (rules.isGameDecided(teamScores)) {
-      var winningTeam = rules.getWinningTeam(teamScores);
+exports.isReady = function(state, req, res) {
+  if (rules.isGameDecided(state.teamScores)) {
+      var winningTeam = rules.getWinningTeam(state.teamScores);
       console.log("Winning team: %j", winningTeam);
-      processWin(winningTeam);
+      processWin(state, winningTeam);
       var jsonResponse = {'response': 'gameDecided', 
-        'scores': {'teamScore': teamScores},
+        'scores': {'teamScore': state.teamScores},
         'winningTeam': winningTeam}
       res(jsonResponse);
   } else {
-    hand = new HandInfo();
-    askPlayers(res);
+    state.hand = new HandInfo();
+    askPlayers(state, res);
   }
 }
 
-exports.makeMove = function(req, res) {
+exports.makeMove = function(state, req, res) {
   var playerId = req['playerId'];
-  var player = getPlayerById(playerId);
+  var player = getPlayerById(state, playerId);
   var playedCard = new Card(req['rank'], req['suit']);
-  var remainingCards = getPlayerCards(player);
+  var remainingCards = getPlayerCards(state, player);
 
   var playerMove = new PlayerMove(player, playedCard);
-  var isValidMove = rules.validatePlayerMove(hand, playerMove, trumpSuit, remainingCards);
+  var isValidMove = rules.validatePlayerMove(state.hand, playerMove, state.trumpSuit, remainingCards);
   if (!isValidMove) {
     var response = {'response': 'invalidMove', 'playerId': req['playerId']}
     res(response);
   } else {
-    hand.addPlayerMove(playerMove);
-    removePlayerCardByMove(playerMove);
-    askPlayers(res);
+    state.hand.addPlayerMove(playerMove);
+    removePlayerCardByMove(state, playerMove);
+    askPlayers(state, res);
   }
 }  
 
-exports.startGame = function(req, res) {
-  resetGame();
-  deck = new Deck();
-  deck.shuffleDeck();
+exports.startGame = function(state, req, res) {
+  resetGame(state);
+  state.deck = new Deck();
+  state.deck.shuffleDeck();
   var teams = [req.playerTeam, req.opponentTeam];
 
   database.incr('game_id', function(err, data) {
     var gameId = data.toString(36);
-    playerList = players.createPlayers(teams);
-    var jsonResponse = {response:'startGame', gameId: gameId, players: playerList, playingOrder: playingOrder};
+    state.playerList = players.createPlayers(teams);
+    var jsonResponse = {response:'startGame', gameId: gameId, players: state.playerList, playingOrder: state.playingOrder};
     res(jsonResponse);
   });
 };
 
-exports.nextGame = function(req, res) {
+exports.nextGame = function(state, req, res) {
   var jsonResponse = {'response': 'nextGame'};
-  jsonResponse['playingOrder'] = playingOrder;
+  jsonResponse['playingOrder'] = state.playingOrder;
   jsonResponse['resultCode'] = 'SUCCESS';
 
-  clearGame();
-  clearTeamScores();
+  clearGame(state);
+  clearTeamScores(state);
 
   res(jsonResponse);
 };
 
 
-exports.dealFirstCards = function(req, res) {
-  _.each(playerList, function(player) {
+exports.dealFirstCards = function(state, req, res) {
+  _.each(state.playerList, function(player) {
     console.log('player %j', player);
-    var newCards = deck.removeCards(5); 
+    var newCards = state.deck.removeCards(5); 
     console.log('player %j adding newCards %j', player, newCards);
-    playerCards[player.id] = newCards;
+    state.playerCards[player.id] = newCards;
   });
 
-  if (!isHumanPlayerFirst()) {
-    trumpSuit = decideTrump();
+  if (!isHumanPlayerFirst(state)) {
+    state.trumpSuit = decideTrump(state);
   }
 
-  var firstCards = playerCards[humanPlayerId];
-  var jsonResponse = { response:'dealFirstCards', cards: firstCards, trumpSuit: trumpSuit};
+  var firstCards = state.playerCards[humanPlayerId];
+  var jsonResponse = { response:'dealFirstCards', cards: firstCards, trumpSuit: state.trumpSuit};
   res(jsonResponse);
 };
 
-exports.chooseTrump = function(req, res) {
-  if (isHumanPlayerFirst()) {
-    trumpSuit = req['suit'];
+exports.chooseTrump = function(state, req, res) {
+  if (isHumanPlayerFirst(state)) {
+    state.trumpSuit = req['suit'];
   }
 
-  while (deck.hasMoreCards()) {
-    _.each(playerList, function(player) {
-      var newCards = deck.removeCards(4);
+  while (state.deck.hasMoreCards()) {
+    _.each(state.playerList, function(player) {
+      var newCards = state.deck.removeCards(4);
       console.log('player %j adding newCards %j', player, newCards);
-      playerCards[player.id] = newCards.concat(playerCards[player.id]); 
+      state.playerCards[player.id] = newCards.concat(state.playerCards[player.id]); 
     });
   } 
 
-  var humanPlayerCards = playerCards[humanPlayerId];
+  var humanPlayerCards = state.playerCards[humanPlayerId];
 
-  var jsonResponse = { response:'allCards', trumpSuit: trumpSuit, cards: humanPlayerCards};
+  var jsonResponse = { response:'allCards', trumpSuit: state.trumpSuit, cards: humanPlayerCards};
   res(jsonResponse);
 
 };
 
 //Utility methods
 
-var isHumanPlayerFirst = function() {
-  console.log("isHumanPlayerFirst playingOrder[0]: %d", playingOrder[0]);
-  console.log("isHumanPlayerFirst 0 == playingOrder[0]: %s", 0 == playingOrder[0]);
-  console.log("isHumanPlayerFirst 0 === playingOrder[0]: %s", 0 === playingOrder[0]);
+var isHumanPlayerFirst = function(state) {
+  console.log("isHumanPlayerFirst playingOrder[0]: %d", state.playingOrder[0]);
+  console.log("isHumanPlayerFirst 0 == playingOrder[0]: %s", 0 == state.playingOrder[0]);
+  console.log("isHumanPlayerFirst 0 === playingOrder[0]: %s", 0 === state.playingOrder[0]);
 
-  return 0 == playingOrder[0];
+  return 0 == state.playingOrder[0];
 };
 
-var getPlayerById = function(id) {
-  var result = _.find(playerList, function(player) {
+var getPlayerById = function(state,id) {
+  var result = _.find(state.playerList, function(player) {
     return player.id == id;
   });
   return result;
 }
 
-var getPlayerByIndex = function(index) {
-  var result = _.find(playerList, function(player) {
+var getPlayerByIndex = function(state, index) {
+  var result = _.find(state.playerList, function(player) {
     return player.index == index;
   });
   return result;
 }
 
-var decideTrump = function() {
-  //var firstPlayerIndex = playingOrder[0];
-  var player = getStartingPlayer();
-  var firstCards = playerCards[player.id];
+var decideTrump = function(state) {
+  var player = getStartingPlayer(state);
+  var firstCards = state.playerCards[player.id];
   return firstCards[0].suit;
 }
 
-var getStartingPlayer = function() {
-  return getPlayerByIndex(playingOrder[0]);
+var getStartingPlayer = function(state) {
+  return getPlayerByIndex(state, state.playingOrder[0]);
 }
 
-var processWin = function(winningTeam) {
-  var currentStartingPlayer = getStartingPlayer()
+var processWin = function(state, winningTeam) {
+  var currentStartingPlayer = getStartingPlayer(state)
     if (currentStartingPlayer.team == winningTeam) {
       console.log("Current starting player is in winning team, not changing playing order")
     } else  {
       console.log("Winning is opposing team, advancing playing order");
-      advancePlayingOrder();
+      advancePlayingOrder(state);
     }
 };
 
-var askPlayers = function(res) {
+var askPlayers = function(state,res) {
   console.log("Asking all players for their move");
   var jsonResponse = {'response': 'handPlayed'}
 
-  while (!hand.isComplete()) {
-    var player = getNextPlayer(hand.getStep());
+  while (!state.hand.isComplete()) {
+    var player = getNextPlayer(state, state.hand.getStep());
     console.log("Asking player with id %s for move", player.id)
 
     // if human, ask asynchronously via websocket
     if (player.isHuman == true) {
       var message = {};
       message['response'] = 'askMove';
-      message['hand'] = convertHand(hand);
+      message['hand'] = convertHand(state, state.hand);
       res(message);
       break;
     } else {
-      var remainingCards = getPlayerCards(player);
-      var card = players.getNextMove(player, remainingCards, hand, trumpSuit);
+      var remainingCards = getPlayerCards(state, player);
+      var card = players.getNextMove(player, remainingCards, state.hand, state.trumpSuit);
 
-      hand.addPlayerMove(new PlayerMove(player, card));
+      state.hand.addPlayerMove(new PlayerMove(player, card));
       console.log("Player with id %s played %j", player.id, card);
 
-      removePlayerCard(player, card);
+      removePlayerCard(state, player, card);
     }
   }
 
-  if (hand.isComplete()) {
+  if (state.hand.isComplete()) {
     console.log("Hand is complete, deciding winner now");
-    var winningMove = rules.decideWinner(hand, trumpSuit);
+    var winningMove = rules.decideWinner(state.hand, state.trumpSuit);
     var winningPlayer = winningMove.player;
 
     console.log("Winner is %s\n", winningPlayer);
 
-    registerWin(winningPlayer);
-    var scoreList = {'teamScore': teamScores};
+    registerWin(state, winningPlayer);
+    var scoreList = {'teamScore': state.teamScores};
 
-    changePlayingOrder(winningPlayer);
+    changePlayingOrder(state, winningPlayer);
 
-    jsonResponse['hand'] = convertHand(hand);
+    jsonResponse['hand'] = convertHand(state, state.hand);
     jsonResponse['winningCard'] = winningMove.card;
     jsonResponse['winningPlayerId'] = winningPlayer.id;
     jsonResponse['scores'] = scoreList;
@@ -213,66 +226,66 @@ var askPlayers = function(res) {
   
 };
 
-var advancePlayingOrder = function() {
-  console.log("oldPlayingOrder %j", playingOrder);
-  var newPlayingOrder = _.rest(playingOrder, 1).concat(_.first(playingOrder));
+var advancePlayingOrder = function(state) {
+  console.log("oldPlayingOrder %j", state.playingOrder);
+  var newPlayingOrder = _.rest(state.playingOrder, 1).concat(_.first(state.playingOrder));
   console.log("newPlayingOrder %j", newPlayingOrder);
-  playingOrder = newPlayingOrder;
+  state.playingOrder = newPlayingOrder;
 };
 
-var changePlayingOrder = function(player) {
-  while (player.index != playingOrder[0]) {
-    advancePlayingOrder(); 
+var changePlayingOrder = function(state,player) {
+  while (player.index != state.playingOrder[0]) {
+    advancePlayingOrder(state); 
   }
 };
 
-var getNextPlayer = function(step) {
+var getNextPlayer = function(state,step) {
   console.log("step %d", step);
-  var index = playingOrder[step];
+  var index = state.playingOrder[step];
   console.log("index %d", index);
-  var player = getPlayerByIndex(index);
+  var player = getPlayerByIndex(state, index);
   console.log("player %j", player);
   return player;
 };
 
-var getPlayerCards = function(player) {
-  var cards = playerCards[player.id];
+var getPlayerCards = function(state, player) {
+  var cards = state.playerCards[player.id];
   return cards;
 };
 
-var removePlayerCard = function(player, card) {
-  var cards = playerCards[player.id];
+var removePlayerCard = function(state, player, card) {
+  var cards = state.playerCards[player.id];
   console.log("player cards size %d", cards.length);
   var filtered = _.filter(cards, function(c) {
     return !(c.rank == card.rank && c.suit == card.suit);
   }); 
   console.log("filtered size %d", filtered.length);
-  playerCards[player.id] = filtered;
+  state.playerCards[player.id] = filtered;
 };
 
-var removePlayerCardByMove = function(playerMove) {
+var removePlayerCardByMove = function(state, playerMove) {
   var player = playerMove.player;
   var card = playerMove.card;
-  removePlayerCard(player, card);
+  removePlayerCard(state, player, card);
 };
 
-var registerWin = function(player) {
+var registerWin = function(state,player) {
   var team = player.team;
-  if (team in teamScores) {
-    teamScores[team] = teamScores[team] + 1;
+  if (team in state.teamScores) {
+    state.teamScores[team] = state.teamScores[team] + 1;
   } else {
-    teamScores[team] = 1;
+    state.teamScores[team] = 1;
   }
 };
 
-var clearTeamScores = function(player) {
+var clearTeamScores = function(state,player) {
   var team;
-  for (team in teamScores) {
-    teamScores[team] = 0;
+  for (team in state.teamScores) {
+    state.teamScores[team] = 0;
   } 
 };
 
-var convertHand = function(hand) {
+var convertHand = function(state, hand) {
   var playerMoves = hand.playerMoves;
   _.map(playerMoves, function(move) {
     move.playerId = move.player.id;
@@ -280,16 +293,17 @@ var convertHand = function(hand) {
   return hand.playerMoves;
 };
 
-var resetGame = function() {
-  playingOrder = [0, 1, 2, 3];
-  playerList = [];
-  clearGame();
+var resetGame = function(state) {
+  state.playingOrder = [0, 1, 2, 3];
+  state.playerList = [];
+  clearGame(state);
 };
 
-var clearGame = function() {
-  deck = new Deck();
-  playerCards = {};
-  teamScores = {};
-  trumpSuit = undefined;
-  hand = new HandInfo();
+var clearGame = function(state) {
+  state.deck = new Deck();
+  state.deck.shuffleDeck();
+  state.playerCards = {};
+  state.teamScores = {};
+  state.trumpSuit = undefined;
+  state.hand = new HandInfo();
 };
--- server/server.js
+++ server/server.js
@@ -3,18 +3,25 @@ var app = require('http').createServer()
   , game = require('./game')
   , config = require('../shared/config')
 
+//Class definitions
+var GameState = game.GameState;
+
+//Config server port
 var port = config.network.port;
 console.log("Server started, listening at port %d", port);
 app.listen(port);
 
+//Start Socket.IO listener
 io.sockets.on('connection', function (socket) {
+  var state = new GameState();
   socket.on('message', function (data) { 
+      
     var json = JSON.parse(data);
     console.log('received message %s', json['command']);
     var command = json['command'] 
     console.log("command %s", command);
     if(typeof game[command] === 'function') {
-      game[command](json, function(response) {
+      game[command](state, json, function(response) {
         var response = JSON.stringify(response);
         socket.send(response);
       }
